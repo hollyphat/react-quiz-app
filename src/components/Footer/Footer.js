2e017227bf1926e845035bfa654be563e3006c38
Olatunji Fatai <sirhollyfat@gmail.com>
2022-02-20 17:23:28 +0100

    Update styles

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        <a
+          href="https://github.com/hollyphat"
+          style={{ cursor: "pointer" }}
+        >
+          Hollyphat
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
